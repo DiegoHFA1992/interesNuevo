75582aab1054a24c6ea28083e0af2cf77c89296f
DiegoHFA <diegourehor@gmail.com>
2015-11-13 12:16:28 +0100

    new file:   .gitignore~

--- Interes.java
+++ Interes.java
@@ -1,10 +1,20 @@
 //fichero Interes.java
-//Clase el interés (I) que produce un capital
-//el cúal  es directamente proporcional al capital inicial (C),
-// al tiempo (t), y a la tasa de interés (i) :
-//I = C · i · t
+/**Clase el interés (I) que produce un capital
+*el cúal  es directamente proporcional al capital inicial (C),
+* al tiempo (t), y a la tasa de interés (i) :
+*I = C · i · t
+*@autor Manuel
+* @version 1.0
+*/
 
 public class Interes{
+	/**metodo calcularInteres que nos sirve para aplicar 
+	 * la formula I=C*i*t
+	 * @param capital, capital inicial
+	 * @param tiempo, tiempo de amortizacion
+	 * @param tasaInteres, tasa interes anual
+	 * @return interes, calculo de la formula
+	 */
 public static double calcularInteres(double capital, double tiempo, double tasaInteres){
    //código para calcular el interes
    //return interes
